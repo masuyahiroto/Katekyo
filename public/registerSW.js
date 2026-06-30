a152ddb8934f9ad24f57fa736363623fe77faf7d
masum <hirotomasuya@gmail.com>
2026-06-30 12:04:17 +0900

    SW kill-switch追加

---
+++ public/registerSW.js
@@ -0,0 +1,6 @@
+// 古いSW登録コードを無効化
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((r) => r.unregister());
+  });
+}
